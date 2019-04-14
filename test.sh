950af3e8427013969b96914efffed93c64c78ad3
Julien M'Poy <julien.mpoy@gmail.com>
2019-04-14 14:59:31 +0200

    Add exit codes to test script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 PROJECT_NAME=testsite
 
-pipenv install django
+pipenv install django || exit 1
 echo 'Creating test django project...'
-pipenv run django-admin startproject "$PROJECT_NAME"
+pipenv run django-admin startproject "$PROJECT_NAME" || exit 1
 echo 'Django project created.'
