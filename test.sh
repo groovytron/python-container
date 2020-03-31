8c4b18d7e24cbaf4a2b5bdfeb3109f7b78b1a94b
Julien M'Poy <julien.mpoy@gmail.com>
2020-03-31 23:10:06 +0200

    Add Python 3.8 image and Poetry to all images

--- test.sh
+++ test.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
 PROJECT_NAME=testsite
+PIPENV_PROJECT_DIR=test_pipenv
+POETRY_PROJECT_DIR=test_poetry
 
 python --version
 python3 --version
 pip --version
 pip3 --version
 pipenv --version
+poetry --version
+
+# Pipenv test
+mkdir "$PIPENV_PROJECT_DIR" && (cd "$PIPENV_PROJECT_DIR" || exit 1)
 
 pipenv install django || exit 1
-echo 'Creating test django project...'
+echo 'Creating test django project with pipenv...'
+
 pipenv run django-admin startproject "$PROJECT_NAME" || exit 1
-echo 'Django project created.'
+echo 'Django project created with pipenv.'
+
+cd ..
+
+# Poetry test
+mkdir "$POETRY_PROJECT_DIR" && (cd "$POETRY_PROJECT_DIR" || exit 1)
+
+poetry init --no-interaction
+
+poetry add Django || exit 1
+echo 'Creating test django project using poetry...'
+
+poetry run django-admin startproject "$PROJECT_NAME" || exit 1
+echo 'Django project created with poetry.'
+
+cd ..
