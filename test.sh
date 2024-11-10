c15f1b099d68f272b883dda14b433c8e2b338cc5
Julien M'Poy <julien.mpoy@gmail.com>
2024-11-10 10:00:06 +0100

    Add GitHub Actions config to build 3.12

--- test.sh
+++ test.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
-PROJECT_NAME=testsite
-PIPENV_PROJECT_DIR=test_pipenv
-POETRY_PROJECT_DIR=test_poetry
-
 python --version
 python3 --version
 pip --version
 pip3 --version
-pipenv --version
 poetry --version
 
-# Create repository folders
-mkdir -p "$PIPENV_PROJECT_DIR" "$POETRY_PROJECT_DIR"
-
-# Pipenv test
-cd "$PIPENV_PROJECT_DIR"
-
-pipenv install django || exit 1
-echo 'Creating test django project with pipenv...'
-
-pipenv run django-admin startproject "$PROJECT_NAME" || exit 1
-echo 'Django project created with pipenv.'
-
-# Poetry test
-cd "../$POETRY_PROJECT_DIR"
-
-poetry init --no-interaction
-
-poetry add Django || exit 1
-echo 'Creating test django project using poetry...'
-
-poetry run django-admin startproject "$PROJECT_NAME" || exit 1
-echo 'Django project created with poetry.'
+# PROJECT_NAME=testsite
+# POETRY_PROJECT_DIR=test_poetry
+# 
+# # Create repository folders
+# mkdir -p "$POETRY_PROJECT_DIR"
+# 
+# # Poetry test
+# cd "$POETRY_PROJECT_DIR" || exit 1
+# 
+# poetry init --no-interaction
+# 
+# poetry add Django || exit 1
+# echo 'Creating test django project using poetry...'
+# 
+# poetry run django-admin startproject "$PROJECT_NAME" || exit 1
+# echo 'Django project created with poetry.'
